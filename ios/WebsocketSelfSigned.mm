c5b8554d19c258ac98053a90ae1d15db6645984b
YUYA KAYAMA <yuyakayama23@gmail.com>
2025-03-21 09:50:52 +0100

    feat: updated native modules

--- ios/WebsocketSelfSigned.mm
+++ ios/WebsocketSelfSigned.mm
@@ -2,10 +2,12 @@
 #import <React/RCTEventEmitter.h>
 
 @interface RCT_EXTERN_MODULE(WebSocketWithSelfSignedCert, RCTEventEmitter)
+
 RCT_EXTERN_METHOD(connect:(NSString *)url
                   headers:(NSDictionary *)headers
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(send:(NSString *)message)
-RCT_EXTERN_METHOD(close)
+RCT_EXTERN_METHOD(send:(NSString *)url message:(NSString *)message)
+RCT_EXTERN_METHOD(close:(NSString *)url)
+
 @end
